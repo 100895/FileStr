decd5f76b284421426d9b9e37b5e73cf858b0153
100895 <dp901q@gmail.com>
2024-07-24 14:21:27 -0400

    Update main.go

--- cmd/fsb/main.go
+++ cmd/fsb/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"EverythingSuckz/fsb/config"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 const versionString = "3.0.0"
-
-// Configuración de la aplicación
-var port string
 
 var rootCmd = &cobra.Command{
 	Use:               "fsb [command]",
@@ -25,33 +22,8 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Run the bot",
-	Run: func(cmd *cobra.Command, args []string) {
-		if port == "" {
-			port = "8080"
-		}
-		fmt.Printf("Starting server on port %s\n", port)
-		// Inicia el servidor en el puerto especificado
-		http.HandleFunc("/", handleRequest)
-		if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
-			fmt.Println("Failed to start server:", err)
-			os.Exit(1)
-		}
-	},
-}
-
-var sessionCmd = &cobra.Command{
-	Use:   "session",
-	Short: "Manage sessions",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Managing sessions...")
-	},
-}
-
 func init() {
-	rootCmd.PersistentFlags().StringVar(&port, "port", "8080", "Port to run the bot on")
+	config.SetFlagsFromConfig(runCmd)
 	rootCmd.AddCommand(runCmd)
 	rootCmd.AddCommand(sessionCmd)
 	rootCmd.SetVersionTemplate(fmt.Sprintf(`Telegram File Stream Bot version %s`, versionString))
@@ -62,10 +34,4 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-// Manejador de solicitudes HTTP
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// Aquí procesas las solicitudes HTTP
-	fmt.Fprintf(w, "Hello, this is your bot responding on port %s!", port)
 }
